82ce31c688cdd4b13c334668700895f2fb234d86
Pumbaa <jakewindsor12@icloud.com>
2024-12-17 01:13:50 +0000

    update: use open sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
+                header: ['"Open Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
             },
             colors: {
                 black: '#131a20',
